63aed768587639267d475b80f128ad0407ada78b
Henadzi Bessonov <henadzi.bessonay@gmail.com>
2026-07-12 10:29:20 +0300

    homework 14 is ready?

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework14/AppArraySizeException.java
@@ -0,0 +1,7 @@
+package ru.otus.java.basic.homeworks.homework14;
+
+public class AppArraySizeException extends RuntimeException {
+    public AppArraySizeException(String message) {
+        super(message);
+    }
+}
